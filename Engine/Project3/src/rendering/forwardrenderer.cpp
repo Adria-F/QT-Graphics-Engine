a7b23c7a24d46fadb2293f213bd11545381186c5
Skyway666 <lucasgarciamateu@gmail.com>
2020-05-03 11:23:37 +0200

    Added black texture

--- Engine/Project3/src/rendering/forwardrenderer.cpp
+++ Engine/Project3/src/rendering/forwardrenderer.cpp
@@ -53,6 +53,7 @@ ForwardRenderer::ForwardRenderer() :
     // List of textures
     addTexture("Final render");
     addTexture("White");
+    addTexture("Black");
 }
 
 ForwardRenderer::~ForwardRenderer()
@@ -272,8 +273,10 @@ void ForwardRenderer::passBlit()
 
         if (shownTexture() == "Final render") {
             gl->glBindTexture(GL_TEXTURE_2D, fboColor);
-        } else {
+        } else if(shownTexture() == "White") {
             gl->glBindTexture(GL_TEXTURE_2D, resourceManager->texWhite->textureId());
+        } else if(shownTexture() == "Black"){
+            gl->glBindTexture(GL_TEXTURE_2D, resourceManager->texBlack->textureId());
         }
 
         resourceManager->quad->submeshes[0]->draw();
