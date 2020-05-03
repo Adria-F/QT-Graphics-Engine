964e791c7720124e7f5c91e7acf8293c94f7ad84
Skyway666 <lucasgarciamateu@gmail.com>
2020-05-03 18:08:54 +0200

    Created deferredrtenderer as a copy of forward renderer

---
+++ Engine/Project3/src/rendering/deferredrenderer.h
@@ -0,0 +1,38 @@
+#ifndef DEFERREDRENDERER_H
+#define DEFERREDRENDERER_H
+
+#include "renderer.h"
+#include "gl.h"
+
+class ShaderProgram;
+class FramebufferObject;
+
+class DeferredRenderer : public Renderer
+{
+public:
+    DeferredRenderer();
+    ~DeferredRenderer() override;
+
+
+    void initialize() override;
+    void finalize() override;
+
+    void resize(int width, int height) override;
+    void render(Camera *camera) override;
+
+private:
+
+    void passMeshes(Camera *camera);
+    void passBlit();
+
+    // Shaders
+    ShaderProgram *forwardProgram = nullptr;
+    ShaderProgram *blitProgram;
+
+    GLuint fboColor = 0;
+    GLuint fboDepth = 0;
+    FramebufferObject *fbo = nullptr;
+
+};
+
+#endif // DEFERREDRENDERER_H
